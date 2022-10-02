9bab856fdd8888bdd0724034f625356b85e54d8f
stalliardo187 <darrenstallard27@hotmail.com>
2022-10-02 11:41:48 +0100

    Finished ui chnages to the footer comp

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,45 +2,46 @@ import React from 'react'
 import { Box, Grid, Typography, Button } from '@mui/material'
 import { Container } from '@mui/system'
 
+
+
 const Footer = () => {
+
+    const handleFooterLinkClicked = () => {
+        // TODO
+        console.log("footer clciked");
+    }
+
+    const FooterLink = (props) => {
+        return (
+            <Typography color="white" sx={{ mr: "20px", width: "fit-content" }} onClick={handleFooterLinkClicked}>{props.text}</Typography>
+        )
+    }
+
     return (
         <Box component="div" sx={{ background: "darkblue", height: "fit-content", width: "100%", mt: "60px" }}>
-            <Container maxWidth="lg" sx={{ height: "inherit", pt: "30px" }}>
-                <Grid container sx={{backgroundColor: "green", flexDirection: {sm: "column", md: "row"}, mb: "20px"}} >
-                    <Grid item container sx={{ mr: "40px", border: "1px solid red", mt: {xs: "30px"} }} xs={12} md={2}>
-                        <Typography color="white" variant='h6'>Site Logo Here</Typography>
-                        <Button sx={{ color: "whitesmoke", width: "105px" }}>Log in</Button>
-                    </Grid>
-                    <Grid item container sx={{ mr: "40px", border: "1px solid red", mt: {xs: "30px"} }} xs={12} md={2}>
+            <Container maxWidth="lg" sx={{ height: "inherit"}}>
+                <Grid container sx={{ flexDirection: { sm: "column", md: "row" }, mb: "20px" }} >
+
+                    <Grid item container sx={{ mr: "40px", mt: { xs: "20px" }, alignItems: "baseline", flexDirection: "column" }} xs={12} md={2}>
                         <Typography color="white" variant='h6'>Contact us</Typography>
-                        <Typography color="white" variant='subtitle2' fontWeight="100">Feel free to get in touch</Typography>
+                        <Typography color="white" variant='subtitle2' fontWeight="100" sx={{ ml: { xs: "20px", md: "0px" } }}>Feel free to get in touch</Typography>
                     </Grid>
-                    <Grid item container sx={{ mr: "40px", border: "1px solid red", mt: {xs: "30px"} }} xs={12} md={2}>
+                    <Grid item container sx={{ mr: "40px", mt: { xs: "20px" }, alignItems: "baseline", flexDirection: "column" }} xs={12} md={2}>
                         <Typography color="white" variant='h6'>About</Typography>
-                        <Typography color="white" variant='subtitle2' fontWeight="100">Find out why we're here</Typography>
+                        <Typography color="white" variant='subtitle2' fontWeight="100" sx={{ ml: { xs: "20px", md: "0px" } }}>Find out what we're about</Typography>
                     </Grid>
                 </Grid>
 
-                {/* <hr style={{ margin: "20px 0 0" }} /> */}
-                
             </Container>
-                <Box component="div" maxWidth="lg" sx={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between", backgroundColor: "darkBlue", margin: "auto", paddingX: "10px", pb: "20px" }}>
-                    <Typography color="white">Privacy Policy</Typography>
-                    <Typography color="white">Terms</Typography>
-                    <Typography color="white">Cookies Settings</Typography>
-                    <Typography color="white">Facebook</Typography>
-                    <Typography color="white">Twitter</Typography>
-                    <Typography color="white">Insta</Typography>
-                    <Typography color="white">Linked In</Typography>
-                </Box>
+            
+
+            <Container maxWidth="lg" sx={{display: "flex", flexDirection: "row", justifyContent: "flex-start"}}>
+                <FooterLink text="Privacy Policy" />
+                <FooterLink text="Terms" />
+                <FooterLink text="Cookie Settings" />
+            </Container>
         </Box>
     )
 }
 
-export default Footer
-
-
-// todos
-// Make a footerLink comp
-// Change the text size of the links on smaller screens
-// Fix layout of links on smaller screens
+export default Footer;
