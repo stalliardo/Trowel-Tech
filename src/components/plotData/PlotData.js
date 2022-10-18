c65dfa2857a60d7072f7619930029cb5140128a9
stalliardo187 <darrenstallard27@hotmail.com>
2022-10-18 15:32:11 +0100

    Added some conditional logic to determine which components to display

--- src/components/plotData/PlotData.js
+++ src/components/plotData/PlotData.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import Home from './Home';
+import Edit from './Edit';
+import CircularIndicator from '../loadingIndicator/CircularIndicator'
 
 const PlotData = () => {
-  return (
-    <div>PlotData</div>
-  )
+
+  const data = false;
+
+  const [isEditing, setIsEditing] = useState(false);
+  const isLoading = false;
+
+  // const isLoading = useSelector(state => state.plotData.isLoading);
+
+  return isLoading ? <CircularIndicator style={{mt: "100px"}}/> : isEditing ? <Edit/> : <Home />;
+    
+  
 }
 
 export default PlotData
