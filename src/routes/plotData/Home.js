f98529fae6292dcbb2917112ebf4efd01a3e3bfb
stalliardo187 <darrenstallard27@hotmail.com>
2022-10-20 10:27:05 +0100

    WIP

--- src/routes/plotData/Home.js
+++ src/routes/plotData/Home.js
@@ -6,14 +6,7 @@ import NoDataPrompt from '../../components/plotData/NoDataPrompt'
 const Home = (props) => {
 
   return (
-
-    
       props.data.length ? <Container>Data found</Container> : <NoDataPrompt />
-    
-    // <div>
-    //   <div>{props.data.length ? "data" : "No Data"}</div>
-    //   <div>{props.hasGangId ? "found" : "No gangId found"}</div>
-    // </div>
   )
 }
 
