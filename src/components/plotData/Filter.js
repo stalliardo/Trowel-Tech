c2486664ced0a2b413d1d242236cf95f2c8e352d
stalliardo187 <darrenstallard27@hotmail.com>
2022-10-23 11:32:14 +0100

    Added styling to the filer component.

--- src/components/plotData/Filter.js
+++ src/components/plotData/Filter.js
@@ -1,8 +1,48 @@
 import React from 'react'
 
+import { Button, Container, Grid, Paper, TextField } from '@mui/material'
+import SelectMenu from '../selectMenu/SelectMenu'
+
+import { STATUS, PLOT_TYPES } from '../../constants/plotData'
+
 const Filter = () => {
+
+  const handleChange = (e) => {
+
+  }
+
   return (
-    <div>Filter</div>
+    <Paper elevation={4}>
+      <Grid container sx={{ padding: "10px 20px 30px", textAlign: "left", justifyContent: "space-between", alignItems: "flex-end"}}>
+        <Grid item xs={12} md={3}>
+          <TextField label="Search Plots" autoFocus sx={{ width: "200px" }} />
+        </Grid>
+
+        <Grid item xs={12} sm={3} sx={{mt: "20px"}}>
+          <SelectMenu
+            label="Category"
+            name="category"
+            menuItems={PLOT_TYPES}
+            handleChange={handleChange}
+            styles={{ width: "200px" }}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={3} sx={{mt: "20px"}}>
+          <SelectMenu
+            label="Status"
+            name="status"
+            menuItems={STATUS}
+            handleChange={handleChange}
+            styles={{ width: "200px" }}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={2}>
+          <Button variant='contained' sx={{mt: "20px"}}>Search</Button>
+        </Grid>
+      </Grid>
+    </Paper>
   )
 }
 
--- src/components/plotData/IndexContainer.js
+++ src/components/plotData/IndexContainer.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import { Container, Typography } from '@mui/material'
+
+import Filter from '../../components/plotData/Filter'
+
 const IndexContainer = () => {
   return (
-    <div>IndexContainer</div>
+    <Container maxWidth="lg" sx={{ backgroundColor: "backDrop.dark", height: "100vh", pt: "20px", mt: "20px" }}>
+        <Typography textAlign="left" variant='h5' mb="15px">Plots</Typography>
+        <Filter />
+  </Container>
   )
 }
 
