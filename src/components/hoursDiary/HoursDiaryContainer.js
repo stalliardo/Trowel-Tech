bb4d4fed53c2c683f75cab8096646648d1c1b5e3
darren <stalliardo187@gmail.com>
2022-11-23 09:35:00 +0000

    wip

--- src/components/hoursDiary/HoursDiaryContainer.js
+++ src/components/hoursDiary/HoursDiaryContainer.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import OutlinedInput from '@mui/material/OutlinedInput';
+
 
 const HoursDiaryContainer = () => {
+
+    const [weekEnding, setWeekEnding] = useState("");
+    const [editDate, setEditDate] = useState(weekEnding ? false : true);
+
+    const handleDateChange = (e) => {
+        console.log('date = ', e.target.value);
+        setWeekEnding(e.target.value);
+        setEditDate(false);
+        
+    }
+
+    // useEffect(() => {
+    //     setWeekEnding()
+    // })
+
   return (
-    <div>HoursDiaryContainer</div>
+    <Box sx={{border: "1px solid green"}}>
+        <Box display="flex">
+           <Box>
+           <Typography textAlign="left" variant='h5'>WeekEnding: </Typography>    
+            {
+                editDate ? <OutlinedInput variant="outlined" type='date' value={weekEnding} onChange={handleDateChange}/> : weekEnding
+            }
+           </Box>
+        </Box>
+    </Box>
   )
 }
 
