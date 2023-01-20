bba021949a7c036fc1abf91c434b0d9e4599856b
stalliardo187 <darrenstallard27@hotmail.com>
2023-01-20 12:39:03 +0000

    Added the extendableTable comp to display the invitations

--- src/components/gangInformation/PendingInvitations.js
+++ src/components/gangInformation/PendingInvitations.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
@@ -6,19 +6,26 @@ import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
 import Paper from '@mui/material/Paper'
 import Button from '@mui/material/Button'
+import ExtendableTable from '../table/ExtendableTable'
+
+
 
 const PendingInvitations = () => {
+    const [tableData, setTableData] = useState({
+        head: ["Recipient Name", "Senders Name", "Status", "Actions"],
+        rows: [{name: "Jamie Gibbs", sender: "Darren Stallard", status: "Pending"}]
+    })
+    const handleCancelInvitation = () => {
+        console.log("cancel invita called");
+    }
     return (
         <Container maxWidth="xl" sx={{ mt: "30px", padding: "20px", backgroundColor: "backDrop.dark", borderRadius: "5px" }}>
             <Box textAlign="left">
                 <Typography variant='h5' textAlign="left" mb="10px">Pending Invitations</Typography>
-                <Paper elevation={4}>
-                    <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "20px" }}>
-                        {/* <TextField label="Members Username" onChange={handleChange} sx={{ width: "30%" }} /> */}
-                        {/* <Button variant="contained" disabled={searchButtonDisabled} onClick={onSearch} sx={{ height: "40px" }}>Search</Button> */}
-                    </Box>
+                {/* <Paper elevation={4}> */}
+                    <ExtendableTable data={tableData} deleteButton={true} handleDelete={handleCancelInvitation}/>
                    
-                </Paper>
+                {/* </Paper> */}
             </Box>
         </Container>
     )
